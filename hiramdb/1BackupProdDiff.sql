8150c7256ac30ca055d6d83245903c68d73ab5a5
Hiram Fleitas <hiramfleitas@hotmail.com>
2023-03-11 18:52:56 -0500

    Update 1BackupProdDiff.sql

--- hiramdb/1BackupProdDiff.sql
+++ hiramdb/1BackupProdDiff.sql
@@ -1,12 +1,11 @@
-exec msdb.dbo.sp_update_job  @job_name = N'WhoIsActive', @enabled = 0;
-exec msdb.dbo.sp_update_job  @job_name = N'Backup.TLogs', @enabled = 0;  
+exec msdb.dbo.sp_update_job  @job_name = N'DatabaseBackup - USER_DATABASES - LOG', @enabled = 0;  
 GO  
 
-if @@servername not in ('FLDSVRSDV01','FLDSVRSQA01')
+if @@servername not in ('hiramdb1')
 begin 
-	if exists (select 1 from msdb.dbo.sysjobs where name ='Backup.Diff') 
+	if exists (select 1 from msdb.dbo.sysjobs where name ='DatabaseBackup - USER_DATABASES - DIFF') 
 	begin
-		exec msdb.dbo.sp_start_job 'Backup.Diff'
+		exec msdb.dbo.sp_start_job 'DatabaseBackup - USER_DATABASES - DIFF'
 	end
 	waitfor delay '00:05:00'
 end

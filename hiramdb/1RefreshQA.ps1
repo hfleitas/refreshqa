850d2a3471d8b38caece60fc80db1297f7a7b8ca
Hiram Fleitas <hiramfleitas@hotmail.com>
2023-03-11 18:59:17 -0500

    Update 1RefreshQA.ps1

--- hiramdb/1RefreshQA.ps1
+++ hiramdb/1RefreshQA.ps1
@@ -1,36 +1,37 @@
 import-module dbatools
-Set-Location c:\fleitasarts\Private\RefreshQA\LicenseManagement
+Set-Location c:\RefreshQA
 
 ## save latest qa backup
-$dir = "\\fldsvm_apps01\SQLSHARED\FLDSVRSQA01\"
-$filter = "LicenseManagement_backup_*.bak"
+$dir = "\\hiramsqlgroupdiag.file.core.windows.net\sqlshared\hiramdb1\hiramdb\FULL\"
+$filter = "*hiramdb*.bak"
 $latest = Get-ChildItem -Path $dir -Filter $filter | Sort-Object LastAccessTime -Descending | Select-Object -First 1
-$dest = "Deployments\"
-$to = ($dir + $dest + $latest.name)
+$dest = "deployments\"
+$to = ("\\hiramsqlgroupdiag.file.core.windows.net\sqlshared\hiramdb1\" + $dest + $latest.name)
 $from = ($dir + $latest.name)
 Move-Item -Path $from -Destination $to
 
+
 # script out permissions
-Invoke-DbaQuery -SqlInstance FLDSVRSQA01 -Database LicenseManagement -File ".\0ScriptOutPermissions.sql" -Verbose -MessagesToOutput | out-file "5ResetDBSecurityPermissions.sql" -force
+Invoke-DbaQuery -SqlInstance hiramdb1 -Database hiramdb -File ".\0ScriptOutPermissions.sql" -Verbose -MessagesToOutput | out-file "5ResetDBSecurityPermissions.sql" -force
 
 #refresh
-Invoke-DbaQuery -SqlInstance FLDASP01_LG01 -File ".\0DisableProdJobs.sql"
-Invoke-DbaQuery -SqlInstance FLDASP01_LG01 -File ".\1BackupProdDiff.sql"
-Invoke-DbaQuery -SqlInstance FLDSVRSQA01 -File ".\1Kill.sql"
+Invoke-DbaQuery -SqlInstance hiramdb2 -File ".\0DisableProdJobs.sql"
+Invoke-DbaQuery -SqlInstance hiramdb2 -File ".\1BackupProdDiff.sql"
+Invoke-DbaQuery -SqlInstance hiramdb1 -File ".\1Kill.sql"
 
 $RestoreTime = Get-Date 
-Get-DbaDbBackupHistory -SqlInstance FLDASP01_LG01 `
--Database LicenseManagement `
--Last | Restore-DbaDatabase -SqlInstance FLDSVRSQA01 `
--DestinationDataDirectory S:\SQLDATA\ `
--DestinationLogDirectory L:\SQLLOGS\ `
+Get-DbaDbBackupHistory -SqlInstance hiramdb2 `
+-Database hiramdb `
+-Last | Restore-DbaDatabase -SqlInstance hiramdb1 `
+-DestinationDataDirectory f:\data\ `
+-DestinationLogDirectory g:\log\ `
 -WithReplace | Format-Table -auto | out-file "OutputRefresh.txt" -Append
 write-host 'STARTED' $RestoreTime ', FINISHED'(get-date)
 
 #reset
-Invoke-DbaQuery -SqlInstance FLDSVRSQA01 -File ".\2TrustWorthyDBOwnerTLogShrink.sql"
-Invoke-DbaQuery -SqlInstance FLDASP01_LG01 -File ".\3EnableProdJobs.sql"
-Invoke-DbaQuery -SqlInstance FLDSVRSQA01 -Database LicenseManagement -File ".\4ResetData.sql"
+Invoke-DbaQuery -SqlInstance hiramdb1 -File ".\2TrustWorthyDBOwnerTLogShrink.sql"
+Invoke-DbaQuery -SqlInstance hiramdb2 -File ".\3EnableProdJobs.sql"
+Invoke-DbaQuery -SqlInstance hiramdb1 -Database hiramdb -File ".\4ResetData.sql"
 
-Invoke-DbaQuery -SqlInstance FLDSVRSQA01 -Database LicenseManagement -File ".\4SyncLogins.sql"
-Invoke-DbaQuery -SqlInstance FLDSVRSQA01 -Database LicenseManagement -File ".\5ResetDBSecurityPermissions.sql"
+Invoke-DbaQuery -SqlInstance hiramdb1 -Database hiramdb -File ".\4SyncLogins.sql"
+Invoke-DbaQuery -SqlInstance hiramdb1 -Database hiramdb -File ".\5ResetDBSecurityPermissions.sql"

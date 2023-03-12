68821c9b13ac5de99940382ec5f798e395060b10
Hiram Fleitas <hiramfleitas@hotmail.com>
2023-03-11 19:31:22 -0500

    Update 3EnableProdJobs.sql

--- hiramdb/3EnableProdJobs.sql
+++ hiramdb/3EnableProdJobs.sql
@@ -1,3 +1,2 @@
-exec msdb.dbo.sp_update_job  @job_name = N'WhoIsActive', @enabled = 1;  
-exec msdb.dbo.sp_update_job  @job_name = N'Backup.TLogs', @enabled = 1;
+exec msdb.dbo.sp_update_job  @job_name = N'DatabaseBackup - USER_DATABASES - LOG', @enabled = 1;
 go

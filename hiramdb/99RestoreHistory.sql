d0ce7c9814e6a67618be94bdca28610e57714139
Hiram Fleitas <hiramfleitas@hotmail.com>
2023-03-11 19:40:05 -0500

    Update 99RestoreHistory.sql

--- hiramdb/99RestoreHistory.sql
+++ hiramdb/99RestoreHistory.sql
@@ -1,18 +1,16 @@
-SELECT
- rh.destination_database_name AS [Database],
-  CASE WHEN rh.restore_type = 'D' THEN 'Database'
-  WHEN rh.restore_type = 'F' THEN 'File'
-   WHEN rh.restore_type = 'I' THEN 'Differential'
-  WHEN rh.restore_type = 'L' THEN 'Log'
-    ELSE rh.restore_type 
- END AS [Restore Type],
- rh.restore_date AS [Restore Date],
- bmf.physical_device_name AS [Source], 
- rf.destination_phys_name AS [Restore File],
-  rh.user_name AS [Restored By]
-FROM msdb.dbo.restorehistory rh
- INNER JOIN msdb.dbo.backupset bs ON rh.backup_set_id = bs.backup_set_id
- INNER JOIN msdb.dbo.restorefile rf ON rh.restore_history_id = rf.restore_history_id
- INNER JOIN msdb.dbo.backupmediafamily bmf ON bmf.media_set_id = bs.media_set_id
-ORDER BY rh.restore_history_id DESC
-GO
+select rh.destination_database_name as Database,
+       case when rh.restore_type = 'D' then 'Database'
+            when rh.restore_type = 'F' then 'File'
+            when rh.restore_type = 'I' then 'Differential'
+            when rh.restore_type = 'L' then 'Log'
+            else rh.restore_type end as RestoreType,
+       rh.restore_date as RestoreDate,
+       bmf.physical_device_name as Source, 
+       rf.destination_phys_name as RestoreFile,
+       rh.user_name as RestoredBy
+from msdb.dbo.restorehistory rh
+inner join msdb.dbo.backupset bs on rh.backup_set_id = bs.backup_set_id
+inner join msdb.dbo.restorefile rf on rh.restore_history_id = rf.restore_history_id
+inner join msdb.dbo.backupmediafamily bmf on bmf.media_set_id = bs.media_set_id
+order by rh.restore_history_id desc
+go
